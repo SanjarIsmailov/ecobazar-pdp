642da9dc3e4b8736d5481c4ed0e0bf58237f3fcd
SanjarPARSIFAL <sanjar666parsifal@gmail.com>
2024-07-29 18:32:03 +0500

    start

---
+++ src/test/java/uz/pdp/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
