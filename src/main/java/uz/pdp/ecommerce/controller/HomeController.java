885ce7d58d4a8c2a0ec17aa55454b1b6604f193a
Sanjar Ismailov <sanjar2008gmail.com>
2024-07-31 14:49:22 +0500

    we must start home controller

---
+++ src/main/java/uz/pdp/ecommerce/controller/HomeController.java
@@ -0,0 +1,12 @@
+package uz.pdp.ecommerce.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/home")
+@RequiredArgsConstructor
+public class HomeController {
+
+}
